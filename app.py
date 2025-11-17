6c69cc3af7c576a9c520ab2d8d2fa74db8c98753
CESARMALCA20 <czar9220@gmail.com>
2025-11-17 08:12:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -745,9 +745,10 @@ m2.metric("🩺 Total Atenciones", f"{total_atenciones:,.0f}")
 # ============================================================
 st.markdown("""
     <div style="text-align: right; margin-top: 40px; color: #555; font-size: 14px;">
-        Elaborado por **César Malca Cabanillas** - Red San Pablo 2025.
+        Red San Pablo 2025.
     </div>
 """, unsafe_allow_html=True)
+
 
 
 
